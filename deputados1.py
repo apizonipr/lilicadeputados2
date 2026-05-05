227658778eac32008227b1512f209977240e495d
alicia <aliciaprevot6@gmail.com>
2026-05-05 15:09:40 -0300

    Update deputados1.py

--- deputados1.py
+++ deputados1.py
@@ -3,25 +3,25 @@ import pandas as pd
 
 df = pd.read_csv('deputados_2022.csv')
 
-col1, col2 = st.columns(2)
-
 st.title("🏛️ Consulta de Deputados por Partido")
+
+col1, col2 = st.columns(2)
 
 with col1:
     lista_partidos = sorted(df['partido'].unique().tolist())
     partido_sel = st.selectbox("Partido:", ["Todos"] + lista_partidos)
 
 with col2:
-    lista_genero = sorted(df['genero'].unique().tolist())
-    genero_sel = st.selectbox("Genero:", ["Todos"] + lista_genero)
-    
+    lista_sexo = sorted(df['sexo'].unique().tolist())
+    sexo_sel = st.selectbox("Sexo:", ["Todos"] + lista_sexo)
+
 df_filtrado = df.copy()
 
 if partido_sel != "Todos":
     df_filtrado = df_filtrado[df_filtrado['partido'] == partido_sel]
 
-if genero_sel != "Todos":
-    df_filtrado = df_filtrado[df_filtrado['genero'] == genero_sel]
-
+if sexo_sel != "Todos":
+    df_filtrado = df_filtrado[df_filtrado['sexo'] == sexo_sel]
+    ---
 st.write(f"Encontrados **{len(df_filtrado)}** deputados com esses critérios.")
 st.dataframe(df_filtrado, use_container_width=True)
